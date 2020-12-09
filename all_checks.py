7da242d26fc4c79b7d8d460c5f59477c19b1b130
Gulgi <01012003ad@gmail.com>
2020-12-09 18:43:52 +0530

    Create wrapper function for check_disk_full. Fix minor error. Add an 'ok' message

--- all_checks.py
+++ all_checks.py
@@ -5,7 +5,7 @@ import shutil
 import sys
 
 def check_reboot():
-    return os.path.exist("/run/reboot-required")
+    return os.path.exists("/run/reboot-required")
 
 def check_disk_full(disk,min_gb,min_percent):
     '''Returns True if there isn't enough disk space, False if otherwise'''
@@ -17,12 +17,18 @@ def check_disk_full(disk,min_gb,min_percent):
     if percent_free < min_percent or gb_free < min_gb:
         return True
     return False
+def check_root_full():
+    '''Returns True if root partition is full, False otherwise'''
+    return  check_disk_full(disk='/',min_gb=2,min_percent=10)
 def main():
     if check_reboot():
         print('Pending Reboot')
         sys.exit(1)
-    if check_disk_full(disk='/',min_gb=2,min_percent=10):
-        print('Disk full')
+    if check_root_full():
+        print('Root partition full')
         sys.exit(1)
+
+    print('Everything is ok')
+    sys.exit(0)
 
 main()
