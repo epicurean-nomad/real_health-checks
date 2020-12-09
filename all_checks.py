efc00e0d6346c685c3d08f74c64db57ab6f190b5
Gulgi <01012003ad@gmail.com>
2020-12-09 19:02:19 +0530

    Allowing to print more than one error message

--- all_checks.py
+++ all_checks.py
@@ -23,10 +23,15 @@ def check_root_full():
     return  check_disk_full(disk='/',min_gb=2,min_percent=10)
 def main():
     checks =[(check_reboot, 'Pending reboot'),(check_root_full,'Root partition full')]
+    everything_ok = True
+
     for check,msg in checks:
         if check():
             print(msg)
-            sys.exit(1)
+            everything_ok = False
+
+    if not everything_ok:
+        sys.exit(1)
 
 
     print('Everything is ok')
