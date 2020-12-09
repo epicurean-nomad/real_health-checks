d21861a6fac9d7083db0c293653aeb1d34108c01
galgishem <74680192+galgishem@users.noreply.github.com>
2020-12-09 14:04:48 +0530

    Finesse

--- all_checks.py
+++ all_checks.py
@@ -13,11 +13,16 @@ def check_disk_full(disk,min_absolute,min_percent):
     #Calculate percentage of free space
     percent_free = 100 * du.free / du.total
     #Calculate how many free gigabytes
+    gb_free = du.free / 2**30
+    if percent_free < min_percent or gb_free < min_absolute:
+        return True
+    return False
 def main():
     if check_reboot():
         print('Pending Reboot')
         sys.exit(1)
-    print('Everything is ok')
-    sys.exit(0)
+    if check_disk_full('/',2,10):
+        print('Disk full')
+        sys.exit(1)
 
 main()
